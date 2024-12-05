79c12e897ccefc1543e25b51dbf940c8c0dd7135
name <mail>
2024-12-05 00:03:15 -0500

    add acc params

--- front/src/lib/pages/accounts_new/ui/AccountNewScreen.svelte
+++ front/src/lib/pages/accounts_new/ui/AccountNewScreen.svelte
@@ -5,12 +5,20 @@
   import { openpgp } from "../../../crypt";
   import { onMount } from "svelte";
   import { routingStore } from "../../../routing/stores";
+    import { submit_stop } from "../../../svelte-default";
+    import { uuidv4 } from "../../../core/uuid";
 
   const labelClass = "mt-3 mb-3";
   const inputClass = "text-slate-800 bg-slate-400";
 
   const fieldLogin = writable();
   const fieldPass = writable();
+  const defaultHttpParam = {
+    url: location.protocol + "//" + location.host,
+    isActive: true,
+    id: uuidv4(),
+  };
+  const fieldHttpServers = writable([defaultHttpParam]);
 
   function submit(e) {
     e.preventDefault();
@@ -19,12 +27,13 @@
     //   name: $fieldLogin,
     //   pass: $fieldPass,
     // };
+    console.log("subbb", $fieldHttpServers);
     appAuthStore.add({
       login: $fieldLogin,
-      pass: $fieldPass
+      pass: $fieldPass,
     });
 
-    routingStore.setPath(ROUTES.ACCOUNTS)
+    routingStore.setPath(ROUTES.ACCOUNTS);
     return;
 
     // TODO: Добавить loading
@@ -38,9 +47,9 @@
       })
       .catch((err) => alert("Err 14211233"));
 
-      // const un = appPassStore.subscribe((val) => {
-      //   console.log({ val });
-      // });
+    // const un = appPassStore.subscribe((val) => {
+    //   console.log({ val });
+    // });
 
     // onMount(() => {
     //   return () => un()
@@ -63,6 +72,57 @@
     <span class="block">pass</span>
     <input bind:value={$fieldPass} class={inputClass} type="password" />
   </label>
+
+  <div class="flex flex-col">
+    <button on:click={(e) => {
+      submit_stop(e);
+      
+      fieldHttpServers.update((prev) => {
+        return [
+          {
+            url: '',
+            id: uuidv4(),
+            isActive: true,
+          },
+          ...prev,
+        ];
+      });
+    }}>Добавить</button>
+    {#each $fieldHttpServers as httpParam, index (httpParam.id)}
+      <hr class="m-4"/>
+      <label class="flex-col flex">
+        <input
+          value={httpParam.url}
+          on:input={(e) => {
+            fieldHttpServers.update((prev) => {
+              return [
+                ...prev.slice(0, index),
+                {
+                  ...httpParam,
+                  url: e.currentTarget.value
+                },
+                ...prev.slice(index+1),
+              ];
+
+            });
+          }}
+        />
+      </label>
+      <label>активность <input checked={httpParam.isActive}
+        on:click={(e) => {
+          fieldHttpServers.update(prev => {
+              return [
+                ...prev.slice(0, index),
+                {
+                  ...httpParam,
+                  isActive: e.currentTarget.checked,
+                },
+                ...prev.slice(index+1),
+              ];
+          })
+      }} type="checkbox" /></label>
+    {/each}
+  </div>
 
   <button
     type="submit"
