a5dad000f9b23e8fbe47059fa52f5d231553e743
name <mail>
2025-06-22 23:07:43 -0400

    add friend indexdb

---
+++ front/src/indexdb/friends/add_friend.ts
@@ -0,0 +1,54 @@
+//import { AES } from "../../../core/crypt";
+import { encrypt_curve25519_from_pass } from "../../core/crypt";
+import { gen_pass } from "../../core/random/gen_pass";
+import { uuidv4 } from "../../core/uuid";
+import { indexdb_wrapper } from "../indexdb_wrapper";
+import { privateKeyToString, recommendedGenerateKeyPair } from "../../libs/libp2p";
+
+export type HttpServerParam = {
+  url: string;
+  isActive: boolean;
+  id: string;
+}
+
+export type FriendEntity = {
+  namePub: string;
+  pass: string;
+  httpServers: HttpServerParam[];
+}
+
+export function add_friend(new_list: FriendEntity[]) {
+  return indexdb_wrapper((db) => {
+    return new Promise(async (res, rej) => {
+      const transaction = db.transaction(["friends"], "readwrite");
+      const store = transaction.objectStore("friends");
+      // Добавляем запись
+      for (let item of new_list) {
+        const newId = uuidv4();
+        //const libp2p_keyPair = await recommendedGenerateKeyPair();
+        
+        const newData = await encrypt_curve25519_from_pass({
+          pass: item.pass,
+          message: JSON.stringify({
+            ...item,
+            id: newId,
+            //_pass: gen_pass(),
+            //_libp2p_keyPair: privateKeyToString(libp2p_keyPair),
+            date_created: new Date(),
+          }),
+        });
+        store.add({ id: newId, data: newData });
+      }
+
+      transaction.oncomplete = function () {
+        console.log("Данные добавлены успешно");
+        res();
+      };
+
+      transaction.onerror = function (event) {
+        console.error("Ошибка при добавлении данных:", event.target.errorCode);
+        rej();
+      };
+    });
+  })
+}
