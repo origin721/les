96691083b4d247008d0f56c1da5b209559082709
name <mail>
2025-06-24 17:56:52 -0400

    routerNewTmp

--- front/src/routing/ui/RoutesView.svelte
+++ front/src/routing/ui/RoutesView.svelte
@@ -3,11 +3,11 @@
         //AccountNewPage,
         AccountsPage,
         //AuthPage,
-        HomePage,
+        //HomePage,
         //Page404,
         SettingsPage,
-        RandomPage,
-        Curve25519Page,
+        //RandomPage,
+        //Curve25519Page,
     } from "../../pages";
     import AesEncrPage from "../../pages/aes_encr_page/ui/AesEncrPage.svelte";
     //import { ChatRoomsPage } from "../../pages/chat_rooms";
@@ -17,9 +17,13 @@
     //import ChatRoomPage from "../../pages/chat_room/ui/ChatRoomPage.svelte";
     //import { ChatRoomsAddPage } from "../../pages/chat_rooms_add";
     import AccountSettingsPage from "../../pages/account_settings/ui/AccountSettingsPage.svelte";
-    import { AddFriendPage } from "../../pages/add_friend_page";
+    //import { AddFriendPage } from "../../pages/add_friend_page";
     import { writableToState } from "../../core/svelte_default/runs/writableToState.svelte";
     import { tick } from "svelte";
+    //import HomePage from "../../pages/home/ui/HomePage.svelte";
+    //import RandomPage from "../../pages/random/ui/RandomPage.svelte";
+    //import AddFriendPage from "../../pages/add_friend_page/ui/AddFriendPage.svelte";
+    //import Curve25519Page from "../../pages/curve25519_page/ui/Curve25519Page.svelte";
     //import AuthPage from "../../pages/auth/ui/AuthPage.svelte";
     //import ChatRoomsAddPage from "../../pages/chat_rooms_add/ui/ChatRoomsAddPage.svelte";
     //import ChatRoomsPage from "../../pages/chat_rooms/ui/ChatRoomsPage.svelte";
@@ -55,6 +59,7 @@
 
     //type = typeName;
 
+    // TODO: сделать защиту от рендера если  данные не менялись сохранив prev в ссылку
     if(p.rState.pathname === ROUTES.ACCOUNTS_NEW) {
       componentPromise = import(`../../pages/accounts_new/ui/AccountNewPage.svelte`);
     }
@@ -73,17 +78,22 @@
     else if(!Object.entries(p.apauState.byId).length) {
         componentPromise = import(`../../pages/auth/ui/AuthPage.svelte`);
     }
+    else if (p.rState.pathname === ROUTES.AUTH) {
+        componentPromise = import(`../../pages/auth/ui/AuthPage.svelte`);
+    }
+    else if (p.rState.pathname === ROUTES.CURVE_25519) {
+        componentPromise = import(`../../pages/curve25519_page/ui/Curve25519Page.svelte`);
+    }
+    else if (p.rState.pathname === ROUTES.ADD_FRIEND) {
+        componentPromise = import(`../../pages/add_friend_page/ui/AddFriendPage.svelte`);
+    }
+    else if (p.rState.pathname === ROUTES.RANDOM) {
+        componentPromise = import(`../../pages/random/ui/RandomPage.svelte`);
+    }
+    else if (p.rState.pathname === ROUTES.HOME) {
+        componentPromise = import(`../../pages/home/ui/HomePage.svelte`);
+    }
 /*
-{:else if routState.state.pathname === ROUTES.AUTH}
-    <AuthPage />
-{:else if routState.state.pathname === ROUTES.CURVE_25519}
-    <Curve25519Page />
-{:else if routState.state.pathname === ROUTES.ADD_FRIEND}
-    <AddFriendPage />
-{:else if routState.state.pathname === ROUTES.RANDOM}
-    <RandomPage />
-{:else if routState.state.pathname === ROUTES.HOME}
-    <HomePage />
 {:else if routState.state.pathname === ROUTES.SETTINGS}
     <SettingsPage />
 {:else if routState.state.pathname === ROUTES.ACCOUNTS}
@@ -99,7 +109,6 @@
    //}
   }
 </script>
-
 
 {#if componentPromise}
   {#await componentPromise then mod}
