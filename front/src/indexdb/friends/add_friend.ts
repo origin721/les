d947339891cee2c803b1d9756768b2b1338d3f8d
ice <mail>
2025-06-27 12:27:47 -0400

    add friend tmp

--- front/src/indexdb/friends/add_friend.ts
+++ front/src/indexdb/friends/add_friend.ts
@@ -4,7 +4,11 @@ import { gen_pass } from "../../core/random/gen_pass";
 import { uuidv4 } from "../../core/uuid";
 import { indexdb_wrapper } from "../indexdb_wrapper";
 import { privateKeyToString, recommendedGenerateKeyPair } from "../../libs/libp2p";
+import { back_store } from "../../local_back/back_store";
 
+export type FriendEntityFull = {
+  id: string;
+} & FriendEntity;
 
 export type FriendEntity = {
   namePub: string;
@@ -12,8 +16,10 @@ export type FriendEntity = {
   myAccId: string;
   friendPubKeyLibp2p: string;
 }
-
-export function add_friend(new_list: FriendEntity[]) {
+// 1. нужно получать myId из аргумента
+export function add_friend(
+  new_list: FriendEntity[],
+) {
   return indexdb_wrapper((db) => {
     return new Promise(async (res, rej) => {
       const transaction = db.transaction(["friends"], "readwrite");
@@ -23,8 +29,9 @@ export function add_friend(new_list: FriendEntity[]) {
         const newId = uuidv4();
         //const libp2p_keyPair = await recommendedGenerateKeyPair();
         
+        const acc = back_store.accounts_by_id[item.myAccId];
         const newData = await encrypt_curve25519_from_pass({
-          pass: item.pass,
+          pass: acc.pass,
           message: JSON.stringify({
             ...item,
             id: newId,
