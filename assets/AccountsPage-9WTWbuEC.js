96560866073e719f5c8446d304a7cce56be98098
origin721 <origin721@users.noreply.github.com>
2025-06-30 04:23:40 +0000

    deploy: ccbbf1ab4982538651e814344f430bb9b141006f

--- assets/AccountsPage-9WTWbuEC.js
+++
@@ -1,1 +0,0 @@
-import{f as o,a as s,a4 as n,e as f,b as i}from"./index-DAcaTA8o.js";import{A as c}from"./AppHeader-BjuHAL2n.js";var m=o("<!> <!>",1);function l(e){var a=m(),r=s(a);c(r,{titleText:"Аккаунты"});var t=f(r,2);n(t,{}),i(e,a)}export{l as default};
