23d860080d93b92a5012f4be82913c9273a4e7ee
origin721 <origin721@users.noreply.github.com>
2025-06-30 03:40:53 +0000

    deploy: f0e2dd548e4e114ff27294ed28d7f692b84b9a0c

--- assets/SettingsPage-DSfPMiPo.js
+++
@@ -1,1 +0,0 @@
-import{f as o,a as s,a3 as i,e as n,b as f}from"./index-DAcaTA8o.js";import{A as m}from"./AppHeader-BjuHAL2n.js";var p=o("<!> <!>",1);function l(t){var a=p(),e=s(a);m(e,{titleText:"Настройки"});var r=n(e,2);i(r,{}),f(t,a)}export{l as default};
