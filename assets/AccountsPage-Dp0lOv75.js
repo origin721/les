b390ef7f364e1f7eaaef4cad4f9541f65de690ef
origin721 <origin721@users.noreply.github.com>
2025-07-03 15:05:51 +0000

    deploy: 666ab49acbbbf6c7090304c870e26f127a0ee5c5

---
+++ assets/AccountsPage-Dp0lOv75.js
@@ -0,0 +1,1 @@
+import{f as o,u as s,aa as n,d as f,a as i}from"./index-clrAq5st.js";import{A as c}from"./AppHeader-CbHraoxN.js";var d=o("<!> <!>",1);function l(t){var a=d(),r=s(a);c(r,{titleText:"Аккаунты"});var e=f(r,2);n(e,{}),i(t,a)}export{l as default};
---
+++ assets/legacy-client-ottpZ9ly.js
@@ -0,0 +1,1 @@
+import{ac as s,ad as f,ae as r,af as t,ag as c,ah as _}from"./index-clrAq5st.js";function o(e){s(()=>{e();var a=f;(a.f&r)!==0&&(t(),c(a,_))})}export{o as r};
