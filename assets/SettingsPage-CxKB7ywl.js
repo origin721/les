a0e31202f6d6315f58d39b034d3cee6dcaab9610
origin721 <origin721@users.noreply.github.com>
2025-07-04 18:59:58 +0000

    deploy: 20b0d8bb618c57931fc6a7ddde9f20157992c3e0

--- assets/SettingsPage-CxKB7ywl.js
+++
@@ -1,1 +0,0 @@
-import{f as i,i as o,a8 as s,m as n,a as f}from"./index-BD3wD_iH.js";import{A as m}from"./AppHeader-Dmr7oExk.js";var p=i("<!> <!>",1);function l(e){var a=p(),t=o(a);m(t,{titleText:"Настройки"});var r=n(t,2);s(r,{}),f(e,a)}export{l as default};
