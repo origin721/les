78c5bf433f62d58d6d6a0b44fedb9dcf3bd1e8f4
ice <mail>
2025-07-05 16:23:27 -0400

    tmp

--- front/src/indexdb/indexdb_wrapper.ts
+++ front/src/indexdb/indexdb_wrapper.ts
@@ -42,7 +42,7 @@ export function indexdb_wrapper(
 
       resultPromise.finally(onFinishOrder);
 
-      let openRequest = indexedDB.open("les_store_v3", 1);
+      let openRequest = indexedDB.open("main_les_store_v1", 1);
 
       openRequest.onupgradeneeded = function (event) {
         const db = openRequest.result;
