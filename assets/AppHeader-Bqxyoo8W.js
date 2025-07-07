2295fea0909188ee2335e35038863ced1d98349e
origin721 <origin721@users.noreply.github.com>
2025-07-07 03:37:04 +0000

    deploy: 310b7d92cb10cb1d5e3444ec06772b4fef60e077

--- assets/AppHeader-Bqxyoo8W.js
+++
@@ -1,1 +0,0 @@
-import{p as l,f as m,i as o,t as d,a as c,y as i,c as f}from"./index-D8BTE75-.js";var n=m('<header data-widget-name="AppHeader" class="pl-3 text-center m-3"> </header> <span class="ml-auto mr-auto mb-3 w-[90%] h-1 bg-slate-700 block"></span>',1);function x(e,t){let r=l(t,"titleText",3,"");var a=n(),s=o(a),p=f(s);d(()=>i(p,r())),c(e,a)}export{x as A};
