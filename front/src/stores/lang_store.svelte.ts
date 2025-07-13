a5caec671e706366186cbd055c8858047eaa8551
ice <mail>
2025-07-12 21:18:04 -0400

    saveLangSave

--- front/src/stores/lang_store.svelte.ts
+++ front/src/stores/lang_store.svelte.ts
@@ -1,11 +1,17 @@
+import { KEYS } from "../core/local-storage/constants";
+
 export const lang_store = (() => {
-  let state = $state();
+  let state = $state<string>(
+    localStorage.getItem(KEYS.LANG)
+    || ''
+  );
 
   return {
     get state() {
       return state;
     },
     set state(newValue: typeof state) {
+      localStorage.setItem(KEYS.LANG, newValue);
       state = newValue;
     }
   }
